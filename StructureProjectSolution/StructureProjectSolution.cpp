f24913fda720ea06055e1fe91d92f7b8579315b5
kamilanov18 <kamilanov18@codingburgas.bg>
2020-04-14 13:13:25 +0300

    Add a data display prototype using HTML and CSS * add web.html

--- StructureProjectSolution/StructureProjectSolution.cpp
+++ StructureProjectSolution/StructureProjectSolution.cpp
@@ -1,9 +1,14 @@
-
+#include "pch.h"
 #include <iostream>
+#include <fstream>
+
+
 using namespace std;
 
 int main()
 {
-    int a;
-    cout << a;
+    ofstream web;
+    web.open("web.html");
+    int a = 1000;
+    web << "<html><body><p style=" << "'color:red'" << ">" << a << "<p><body>";
 }
